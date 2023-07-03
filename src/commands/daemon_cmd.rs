3a69e24c247303c7c6b2e416f22c39c779258aae
Dan Pashin <danypashin@yandex.ru>
2023-07-03 20:33:20 +0300

    Fix not updating lists at start

--- src/commands/daemon_cmd.rs
+++ src/commands/daemon_cmd.rs
@@ -1,7 +1,6 @@
 use super::{update_cmd::Command as UpdateCommand, CliCommand, GlobalOptions};
 use crate::source::SourcesCache;
 use anyhow::Result;
-use std::time::Duration;
 
 #[derive(clap::Parser)]
 pub(crate) struct Command {
@@ -13,12 +12,12 @@ pub(crate) struct Command {
 impl CliCommand for Command {
     async fn run(&self) -> Result<()> {
         let config = self.global_options.parse_config()?;
-        let sleep_duration: Duration = config.auto_update.timeout.into();
 
         let shutdown = tokio_shutdown::Shutdown::new()?;
         let update_cmd = UpdateCommand::new(self.global_options.clone());
 
         let cache = SourcesCache::new();
+        let mut update_interval = tokio::time::interval(config.auto_update.timeout.into());
 
         eprintln!("Starting daemon...");
         loop {
@@ -27,7 +26,7 @@ impl CliCommand for Command {
                         eprintln!("Got shutdown signal. Exiting...");
                         break;
                     },
-                _ = tokio::time::sleep(sleep_duration) => {
+                _ = update_interval.tick() => {
                     if let Err(error) = update_cmd.perform_update(&config, cache.clone()).await {
                         eprintln!("Error when performing update command: {error:?}");
                     }
